2ed8bfa3ac19ffcc127b0b2849a3916cfcea41e2
matthew jordan <matthewsages@gmail.com>
2021-01-03 06:39:02 -0800

    Cleanup / Add figs

--- main.py
+++ main.py
@@ -9,33 +9,7 @@ import tqdm
 from dataclasses import dataclass
 
 
-def read_ubyte(filepath, is_img = True, image_size = 28, num_samples = 10000):
-    f = gzip.open(filepath, "r")
-    if (is_img):
-        num_samples = 3
-        buf = f.read(image_size * image_size * num_samples)
-        data = np.frombuffer(buf, dtype = np.uint8).astype(np.float32)
-        data = data.reshape(num_samples, image_size, image_size)
-
-        # plt.imshow(data[2], cmap="gray")
-        # plt.show()
-        # exit()
-
-
-    else:
-        buf = f.read(num_samples)
-        data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
-
-
-    return(data.squeeze())
-
-
-def ReLU(x):
-    return(x if x > 0 else 0)
-
-
 def sigmoid(x):
-    # s = lambda k: 1 / (1 + (math.e ** (-xi)))
     return(np.array([1 / (1 + (math.e ** (-xi))) for xi in x]))
 
 
@@ -88,7 +62,6 @@ class LinearLayer():
 
     def __post_init__(self):
         # For each node in output layer, generate empty weights and biases
-        # self.weights = np.random.uniform(0, 1, size =[self.out_size, self.in_size])
         self.weights = np.random.randn(self.out_size, self.in_size) * \
                 np.sqrt(2 / self.in_size)
         self.biases = np.random.uniform(0, 1, self.out_size)
@@ -130,16 +103,11 @@ class Net():
         t = np.zeros(len(pred))
         t[actual] = 1
 
-        # G = pred - t
-        # G = (pred - t) / (pred * (np.ones(len(pred)) - pred))
-        # S = sigprime(self.L2.z)
-        # D2 = np.multiply(G, S)
-
-        # D1 = np.multiply(np.dot(self.L2.weights.T, D2), sigprime(self.L1.z))
-
+        # Find the error at each layer
         D1 = self.delta(0, t)
         D2 = self.delta(1, t)
 
+        # Update each layer given the error at each layer
         for i in range(50):
             for j in range(784):
                 self.L1.weights[i][j] -= D1[i] * self.L1.X[j] * alpha
@@ -147,9 +115,6 @@ class Net():
         for i in range(10):
             for j in range(50):
                 self.L2.weights[i][j] -= D2[i] * self.L2.X[j] * alpha
-
-        # self.L1.weights -= D1 * self.L2.X
-        # self.L1.weights -= D1 * self.L1.X
 
         self.L1.biases -= D1 * alpha
         self.L2.biases -= D2 * alpha
@@ -170,57 +135,96 @@ class Net():
         return np.multiply(np.dot(w.T, self.delta(l + 1, t)), dA)
 
 
-def loss(pred, actual, net):
-    alpha = 0.01
-    loss = 0
-    if actual > 9:
-        return 0
+    def loss(self, pred, actual):
+        """ Binary cross entropy loss.
+            Function: −(𝑦log(𝑝)+(1−𝑦)log(1−𝑝))"""
+        alpha = 0.01
+        loss = 0
+        if actual > 9:
+            return 0
 
-    t = np.zeros(len(pred))
-    t[actual] = 1
+        t = np.zeros(len(pred))
+        t[actual] = 1
 
-    # Binary cross entropy loss
-    # Function: −(𝑦log(𝑝)+(1−𝑦)log(1−𝑝))
-    for i in range(len(t)):
-        loss -= t[i] * math.log(pred[i]) + (1 - t[i]) * math.log(1 - pred[i])
+        for i in range(len(t)):
+            loss -= t[i] * math.log(pred[i]) + (1 - t[i]) * math.log(1 - pred[i])
 
-    return(loss)
+        return(loss)
 
 
 if __name__=="__main__":
     data_train = pd.read_csv("data/train.csv")
+    data_test = pd.read_csv("data/test.csv")
 
     train_labels = np.array(data_train.iloc[:, 0])
-    train_images = np.array(data_train.iloc[:, 1:]).reshape(42000, 28, 28)
+    train_images = np.array(data_train.iloc[:, 1:]).reshape(data_train.shape[0], 28, 28)
 
-    train_data = MnistDataLoader(train_images[:500], train_labels[:500])
+    test_labels = np.array(data_test.iloc[:, 0])
+    test_images = np.array(data_test.iloc[:, 1:]).reshape(data_test.shape[0], 28, 28)
+
+    n = 1000
+    train_data = MnistDataLoader(train_images[:n], train_labels[:n])
+    test_data = MnistDataLoader(test_images[:n], test_labels[:n])
     net = Net()
 
     train_accuracy = []
     train_loss = []
-    for epoch in range(20):
+    test_accuracy = []
+    test_loss = []
+    for epoch in range(12):
         correct = 0
         counter = 0
         running_loss = 0
-        for image in tqdm.tqdm(train_data):
-            result = net.forward(image["image"])
+        print("Epoch:", epoch + 1)
+        for i, image in enumerate(tqdm.tqdm(train_data)):
+            f = normalize(image["image"])
+            result = net.forward(f)
 
-            running_loss += loss(result, image["label"], net)
+            running_loss += net.loss(result, image["label"])
             net.backprop(result, image["label"])
 
             if image["label"] == np.argmax(result):
                 correct += 1
-
             counter += 1
 
-        train_accuracy.append(correct / counter)
-        train_loss.append(running_loss)
+            if (i + 1) % 20 == 0:
+                train_accuracy.append(correct / counter)
+                train_loss.append(running_loss)
+                running_loss = 0
 
-        print("Epoch:", epoch)
-        print("Accuracy:", correct / counter)
-        print("Loss:", running_loss)
+        print("Train Accuracy:", correct / counter)
+        print("Train Loss:", running_loss)
 
-    plt.plot(normalize(np.array(train_accuracy)))
-    plt.plot(normalize(np.array(train_loss)))
+        correct = 0
+        counter = 0
+        running_loss = 0
+        for i, image in enumerate(test_data):
+            f = normalize(image["image"])
+            result = net.forward(f)
+
+            running_loss += net.loss(result, image["label"])
+
+            if image["label"] == np.argmax(result):
+                correct += 1
+            counter += 1
+
+            if (i + 1) % 20 == 0:
+                test_accuracy.append(correct / counter)
+                test_loss.append(running_loss)
+                running_loss = 0
+
+        print("Test Accuracy:", correct / counter)
+        print("Test Loss:", running_loss)
+        print()
+
+    plot1 = plt.figure(1)
+    plt.plot(normalize(np.array(train_accuracy)), label="Train accuracy")
+    plt.plot(normalize(np.array(test_accuracy)), label="Test accuracy")
+    plt.legend(loc="upper left")
+
+    plot2 = plt.figure(2)
+    plt.plot(normalize(np.array(train_loss)), label="Train loss")
+    plt.plot(normalize(np.array(test_loss)), label="Test loss")
+    plt.legend(loc="upper left")
+
     plt.show()
-
